cef9657f3a2f825160276841b952cdf0017fecdc
Michel Perrocheau <mperrocheau>
2021-03-03 17:16:20 +0100

    [IMP] migrate base_automation

---
+++ openupgrade_scripts/scripts/base_automation/14.0.1.0/pre-migrate.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Odoo Community Association (OCA)
+# Copyright (C) 2021 - Teclib'ERP (<https://www.teclib-erp.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import logging
+
+from openupgradelib import openupgrade
+
+from odoo import tools
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    
+    # Fix error on _process_ondelete
+    cr.execute(
+        """ DELETE FROM ir_model WHERE model='base.automation.lead.test';
+    """
+    )
